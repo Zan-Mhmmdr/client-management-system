f76148ae3f1157b843d516136e8ce8cf26e2db0d
ezann <rayzanmuhammadr774@gmail.com>
2025-08-19 23:29:07 +0700

    add: api for register user

---
+++ src/features/authentication/services/authServices.ts
@@ -0,0 +1,16 @@
+export const userRegister = async (user: {
+  username: string;
+  name: string;
+  password: string;
+}) => {
+  return await fetch(`${import.meta.env.VITE_API_PATH}/users`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+};
+
+export const userLogin = async () => {};
