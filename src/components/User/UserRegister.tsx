ecbd8508de3ae05f3a7a040ca875e283bba6c6f1
ezann <rayzanmuhammadr774@gmail.com>
2025-08-19 17:49:38 +0700

    feat: register page

--- src/components/User/UserRegister.tsx
+++ src/components/User/UserRegister.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardAction,
@@ -7,30 +7,40 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Link } from "react-router-dom"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
 
-export function UserLogin() {
+export function UserRegister() {
   return (
     <Card className="w-full max-w-sm bg-[#0F0F0F] text-[#EAEAEA] border border-[#008b8b] shadow-[0_0_20px_#aec8d7]">
       <CardHeader>
         <CardTitle className="text-xl font-semibold text-white">
-          Login
+          Register
         </CardTitle>
         <CardDescription className="text-sm text-[#888]">
-          Enter your email and password
+          Create your account to get started
         </CardDescription>
         <CardAction>
-          <Link to="/register" className="text-white p-0 h-auto text-sm">
-            Sign Up
+          <Link to="/login" className="text-white p-0 h-auto text-sm">
+            Sign In
           </Link>
         </CardAction>
       </CardHeader>
       <CardContent>
         <form>
           <div className="flex flex-col gap-4">
+            <div className="grid gap-1">
+              <Label htmlFor="username">Username</Label>
+              <Input
+                id="username"
+                type="text"
+                placeholder="Username"
+                required
+                className="bg-[#1A1A1A] border border-[#333] text-white placeholder-gray-500"
+              />
+            </div>
             <div className="grid gap-1">
               <Label htmlFor="email">Email</Label>
               <Input
@@ -43,6 +53,15 @@ export function UserLogin() {
             </div>
             <div className="grid gap-1">
               <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                required
+                className="bg-[#1A1A1A] border border-[#333] text-white"
+              />
+            </div>
+            <div className="grid gap-1">
+              <Label htmlFor="password">Confirm password</Label>
               <Input
                 id="password"
                 type="password"
@@ -68,7 +87,7 @@ export function UserLogin() {
         </Button>
       </CardFooter>
     </Card>
-  )
+  );
 }
 
-export default UserLogin
+export default UserRegister;
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./style/index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import UserLogin from "./components/User/UserLogin";
 import "./style/index.css";
+import UserRegister from "./components/User/UserRegister";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<UserLogin />} />
+          <Route path="/register" element={<UserRegister />} />
         </Route>
       </Routes>
     </BrowserRouter>
